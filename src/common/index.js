7d5d815cf7607519549c33126808cb82ced69bdc
CodeBear <249900679@qq.com>
2018-04-26 10:55:27 +0800

    [修改] 全局引入自定义组件 可排除指定的组件

--- src/common/index.js
+++ src/common/index.js
@@ -4,17 +4,26 @@ function capitalizeFirstLetter (string) {
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
+// 排除要全局引入的组件,可以是目录名也可以是文件名
+const exclude = [
+  'pageDesign/widgets'
+]
+
+const regex = RegExp('.*^(?!.*?(' + exclude.join('|') + ')).*\\.vue$')
+
 const requireComponent = require.context(
   // 找到components文件夹下以.vue命名的文件
   '.', true, /\.vue$/
 )
 
 requireComponent.keys().forEach(fileName => {
-  const componentConfig = requireComponent(fileName)
+  if (regex.test(fileName)) {
+    const componentConfig = requireComponent(fileName)
 
-  const componentName = capitalizeFirstLetter(
-    // 因为得到的filename格式是: './baseButton/baseButton.vue', 所以这里我们去掉头和尾，只保留真正的文件名
-    fileName.replace(/^\..*\//, '').replace(/\.\w+$/, '')
-  )
-  Vue.component(componentName, componentConfig.default || componentConfig)
+    const componentName = capitalizeFirstLetter(
+      // 因为得到的filename格式是: './baseButton/baseButton.vue', 所以这里我们去掉头和尾，只保留真正的文件名
+      fileName.replace(/^\..*\//, '').replace(/\.\w+$/, '')
+    )
+    Vue.component(componentName, componentConfig.default || componentConfig)
+  }
 })
